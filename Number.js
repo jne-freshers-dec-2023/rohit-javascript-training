f71ba594ff372ff008bcb262238c425b62131779
Rohit Lasurkar <rohit.lasurkar@thinkitive.com>
2024-03-07 18:33:49 +0530

    Number

---
+++ Number.js
@@ -0,0 +1,37 @@
+let num=100;
+console.log(num);
+
+let num1 = 071;
+console.log(num1);
+
+let num2=080;
+console.log(num2); //it ignore 0 
+
+//ES6
+ 
+let a= 0o71;
+console.log(a);
+
+// let b= 0o80;
+// console.log(b);
+
+let z=0x1f;
+console.log(z);
+
+//floating- point 
+
+let a1=9.99424242324324;
+console.log(a1);
+
+let a2=39.3902e1;
+console.log(a2);
+
+let a3=334e-2;
+console.log(a3);
+
+
+let amount = 0.2 + 0.1;
+console.log(amount);
+
+let account=293090923911928921n
+console.log(account);
