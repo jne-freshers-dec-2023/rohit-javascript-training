3989623458b2494f2a4941a20edda5839c8c2905
Rohit Lasurkar <rohit.lasurkar@thinkitive.com>
2024-03-07 19:43:42 +0530

    Syntax

---
+++ syntax.js
@@ -0,0 +1,26 @@
+//not used white space
+let formatted = true; if (formatted) {console.log('The code is easy to read');}
+
+//used white space
+let a=true;
+
+if(a)
+{
+    console.log("Hello");
+}
+
+//Statement
+
+let message = "Welcome to JavaScript";
+console.log(message);
+
+//Expression
+
+console.log(2+1);
+
+
+
+
+
+
+  
