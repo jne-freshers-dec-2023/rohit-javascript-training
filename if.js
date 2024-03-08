f3bcb08495f9a903002319e63ea1cb5d5a3339dc
Rohit Lasurkar <rohit.lasurkar@thinkitive.com>
2024-03-08 19:33:34 +0530

    control flow statement and loop statement

---
+++ if.js
@@ -0,0 +1,11 @@
+let age = 18;
+if (age >= 18) {
+  console.log('You can sign up');
+}
+
+let age1 = 16;
+let state = 'CA';
+
+if (state == 'CA' && age1 == 16) {
+  console.log('You can drive.');
+}
